0f6ae9ead622d3ad77acbefaeec1a99e5bc2f031
MarianaFigueras <marianafigueras58@gmail.com>
2022-10-13 14:36:47 -0300

    base dir

--- entregaInt/views.py
+++ entregaInt/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 # from datetime import datetime
 from django.template import Context, Template, loader
+import random
 
 def hola(request):
     return HttpResponse('<h1> Bienvenido </h1>')
@@ -17,10 +18,14 @@ def mi_template(request):
 
 def tu_template(request, nombre):
     
-    cargar_archivo = open(r'C:\Users\mariana figueras\Desktop\Código\Proyecto Grupal\templates\tu_template.html', 'r')
-    template = Template(cargar_archivo.read())
-    cargar_archivo.close()
-    contexto = Context({'pedido':nombre})
-    template_renderizado = template.render(contexto)
+    # cargar_archivo = open(r'C:\Users\mariana figueras\Desktop\Código\Proyecto Grupal\templates\tu_template.html', 'r')
+    # template = Template(cargar_archivo.read())
+    # cargar_archivo.close()
+    # contexto = Context({'pedido':nombre})
+    # template_renderizado = template.render(contexto)
+    
+
+    template = loader.get_template('tu_template.html')
+    template_renderizado = template.render({'pedido': nombre})
         
     return HttpResponse(template_renderizado)
