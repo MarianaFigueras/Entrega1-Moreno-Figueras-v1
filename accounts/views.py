3b397bc70f33878f28b6f7812168bf5a12e49ae8
LuisMoreno <luisjmj.1@gmail.com>
2022-11-27 19:50:57 -0300

    Se edita informacion de Usuario

--- accounts/views.py
+++ accounts/views.py
@@ -54,6 +54,7 @@ def editar_perfil(request):
            user.first_name = data_nueva['first_name']
            user.last_name = data_nueva['last_name']
            user.email = data_nueva['email']
+           user.genero = data_nueva['genero']
            user.extensionusuario.avatar = data_nueva['avatar']
            
            user.extensionusuario.save()          
